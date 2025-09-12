c3c7ab4b71907e97503450cd1f6bb7e9bfb90a25
kovacsrud <kovacs.rud@gmail.com>
2025-09-12 12:03:09 +0200

    Első react app

---
+++ react_elso_app/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  
+
+  return (
+    <div>
+       <Header headerText="Első react alkalmazás" headerEnd=" Vite alkalmazásával" />
+    </div>
+  )
+}
+
+export default App
---
+++ react_elso_app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+
+function Header({headerText,headerEnd}) {
+  return (
+    <h1>{headerText} {headerEnd}</h1>
+  )
+}
+
+export default Header
