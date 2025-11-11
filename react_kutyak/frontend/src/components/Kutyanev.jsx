2434cd9e86a9093ce08626aa8c62c3e6062ffac9
kovacsrud <kovacs.rud@gmail.com>
2025-11-11 10:39:34 +0100

    Kutyaform implementáció

--- react_kutyak/frontend/src/components/Kutyanev.jsx
+++ react_kutyak/frontend/src/components/Kutyanev.jsx
@@ -1,11 +1,32 @@
 import {useNavigate} from 'react-router-dom';
 import {useContext,useState} from 'react';
 import KutyaContext from '../context/KutyaContext';
-
+import Modal from './Modal';
+import Modal2 from './Modal2';
 function Kutyanev({ kutyanev }) {
 
   const navigate=useNavigate();
   const {backendMuvelet}=useContext(KutyaContext);
+
+  const[isDelete,setIsDelete]=useState(false);
+  const[isModify,setIsModify]=useState(false);
+
+  const openDelete=()=>{
+    setIsDelete(true);
+  }
+
+  const closeDelete=()=>{
+    setIsDelete(false);
+  }
+
+  const openModify=()=>{
+    setIsModify(true);
+  }
+
+  const closeModify=()=>{
+    setIsModify(false);
+  }
+
 
   const modosit=()=>{
     navigate('/kutyanevform',{state:{kutyanev}})
@@ -18,20 +39,30 @@ function Kutyanev({ kutyanev }) {
 
 
   return (
+    <div>
+      
     <div className="card m-5 w-96 bg-lime-100 text-lime-800 card-md shadow-sm">
+      {
+        isModify && (<Modal2 title="Módosítás" body="Biztosan módosítja?" commitFunction={()=>modosit()} closeFunction={()=>closeModify()} />)
+      }
+      {
+        isDelete && (<Modal2 title="Törlés" body="Biztosan törli?" commitFunction={()=>torol()} closeFunction={()=>closeDelete()} />)
+      }
+      
       <div className="card-body">
         <h2 className="card-title">{kutyanev.kutyanev}</h2>
         <p>
           Id:{kutyanev.Id}
         </p>
         <div className="justify-end card-actions">
-          <button onClick={()=>modosit()} className="btn btn-primary">Módosítás</button>
-          <button onClick={()=>torol()} className="btn btn-primary">Törlés</button>
+          <button onClick={openModify} className="btn btn-primary">Módosítás</button>
+          <button onClick={openDelete} className="btn btn-primary">Törlés</button>
         </div>
         <div className="justify-end card-actions">
           
         </div>
       </div>
+    </div>
     </div>
   );
 }
