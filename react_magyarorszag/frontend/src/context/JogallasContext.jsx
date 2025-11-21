fdba433f37feeed001c37a3b4375e2be8c65c65f
kovacsrud <kovacs.rud@gmail.com>
2025-11-21 12:41:37 +0100

    JogallasContext létrehozása

--- react_magyarorszag/frontend/src/context/JogallasContext.jsx
+++ react_magyarorszag/frontend/src/context/JogallasContext.jsx
@@ -3,13 +3,39 @@ import { useState,useEffect,createContext  } from "react";
 const JogallasContext=createContext();
 
 export const JogallasProvider=({children})=>{
+    const[jogallasok,setJogallasok]=useState([]);
+    const[jogallasTelepulesek,setJogallasTelepulesek]=useState([]);
+    const[kivalasztottJogallas,setKivalasztottJogallas]=useState("");
 
+    const getJogallasok=()=>{
+         fetch(`${import.meta.env.VITE_BASE_URL}/jogallasok`)
+        .then(res=>res.json())
+        .then(adat=>setJogallasok(adat))
+        .catch(err=>alert(err));
 
+    }
 
-    return <JogallasContext.Provider value={{}}>{children}</JogallasContext.Provider>
+    const getJogallasTelepulesei=(jogallas)=>{
+        fetch(`${import.meta.env.VITE_BASE_URL}/jogallasok/jogallas/${jogallas}`)
+        .then(res=>res.json())
+        .then(adat=>setJogallasTelepulesek(adat))
+        .catch(err=>alert(err));
+
+    }
+
+     useEffect(()=>{
+        getJogallasok();
+        if(kivalasztottJogallas!=""){
+            getJogallasTelepulesei(kivalasztottJogallas);
+        }
+    },[kivalasztottJogallas])
+
+    return <JogallasContext.Provider value={{
+        jogallasok,
+        jogallasTelepulesek,
+        kivalasztottJogallas,
+        setKivalasztottJogallas
+    }}>{children}</JogallasContext.Provider>
 }
-
-
-
 
 export default JogallasContext;
