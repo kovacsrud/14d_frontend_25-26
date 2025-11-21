197714f2fffa2a41a9c7b3507472a623e6f863e8
kovacsrud <kovacs.rud@gmail.com>
2025-11-21 08:49:46 +0100

    Településadatok megjelenítése

--- react_magyarorszag/frontend/src/components/TelepulesAdatok.jsx
+++ react_magyarorszag/frontend/src/components/TelepulesAdatok.jsx
@@ -2,12 +2,65 @@ import { useContext } from "react";
 import TelepulesContext from "../context/TelepulesContext";
 
 function TelepulesAdatok() {
-    const {telepules,selectedTelepules}=useContext(TelepulesContext);
+  const { telepules, selectedTelepules } = useContext(TelepulesContext);
   return (
     <div>
-        <h1 className="m-5 text-3xl text-center font-bold text-amber-700">{selectedTelepules} {telepules.telepulesnev}</h1>
+      <h1 className="m-5 text-3xl text-center font-bold text-amber-700">
+        {telepules.telepulesnev}
+      </h1>
+
+      <div class="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg">
+        <div class="px-4 py-5 sm:px-6">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">
+            {telepules.telepulesnev}
+          </h3>
+          <p class="mt-1 max-w-2xl text-sm text-gray-500">
+            {telepules.telepulesnev} fontosabb adatai:
+          </p>
+        </div>
+        <div class="border-t border-gray-200">
+          <dl>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Település neve:</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {telepules.telepulesnev}
+              </dd>
+            </div>
+            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Megye:</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {telepules.megyenev}
+              </dd>
+            </div>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Jogállás:</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {telepules.jogallas}
+              </dd>
+            </div>
+            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Terület:</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {telepules.terulet}
+              </dd>
+            </div>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Lakások:</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {telepules.lakasok}
+              </dd>
+            </div>
+            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt class="text-sm font-medium text-gray-500">Koordináták:</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                lat:{telepules.lat}, lng:{telepules.lng}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TelepulesAdatok
+export default TelepulesAdatok;
