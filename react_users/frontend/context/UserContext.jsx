4afb7dd485fdd0349fed99d7d6ec3ec33f449d8b
kovacsrud <kovacs.rud@gmail.com>
2025-12-10 09:10:00 +0100

    Frontend app felhasználó kezeléssel, context létrehozása

---
+++ react_users/frontend/context/UserContext.jsx
@@ -0,0 +1,24 @@
+import { useState,createContext } from "react";
+
+const UserContext=createContext();
+
+export const UserProvider=({children})=>{
+    const[refresh,setRefresh]=useState(false);
+
+    const update=()=>{
+        setRefresh(prev=>!prev);
+    }
+
+    const logout=()=>{
+        sessionStorage.removeItem('usertoken');        
+    }
+
+
+    return <UserContext.Provider value={{
+        refresh,
+        update,
+        logout
+    }}>{children}</UserContext.Provider>
+}
+
+export default UserContext;
