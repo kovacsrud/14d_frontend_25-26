8bf6dfe56edb0d55b0eb466fe44dbc6d442fbf75
kovacsrud <kovacs.rud@gmail.com>
2025-12-10 10:01:16 +0100

    Főoldal és a menü elkészítése

--- react_users/frontend/src/components/Menu.jsx
+++ react_users/frontend/src/components/Menu.jsx
@@ -1,9 +1,23 @@
-
+import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <div>Menu</div>
-  )
+    <div className="navbar bg-lime-300 shadow-sm">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">Main</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+           <li>
+            <Link to="/register">Register</Link>
+          </li>                    
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
