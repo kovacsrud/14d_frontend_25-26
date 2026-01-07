afa6604c7fac929207e67ae52ff6841ba75c13d4
kovacsrud <kovacs.rud@gmail.com>
2026-01-07 09:53:00 +0100

    SubHeader komponens

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import SubHeader from "./components/SubHeader"
 
 function App() {
  
@@ -7,6 +8,10 @@ function App() {
     <div>
       <h1 className="text-3xl text-center text-sky-800">React+TypeScript</h1>            
       <Header headerText="TypeScript H1" id={4} />
+      <SubHeader>
+         <p>Bekezdés 1.</p>
+         <p>Bekezdés 2.</p>
+      </SubHeader>
     </div>
   )
 }
