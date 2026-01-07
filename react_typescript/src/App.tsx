a462d48a65c149245bf196c38bf853b23e9f2bef
kovacsrud <kovacs.rud@gmail.com>
2026-01-07 09:37:00 +0100

    React Typescript

---
+++ react_typescript/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-sky-800">React+TypeScript</h1>            
+    </div>
+  )
+}
+
+export default App
