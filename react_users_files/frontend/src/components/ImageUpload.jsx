923c518ef682b27e25d4603162f1703af3358838
kovacsrud <kovacs.rud@gmail.com>
2026-02-12 13:19:56 +0100

    Kép feltöltése

--- react_users_files/frontend/src/components/ImageUpload.jsx
+++ react_users_files/frontend/src/components/ImageUpload.jsx
@@ -1,8 +1,60 @@
-
+import { useState,useEffect,useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../context/UserContext";
 
 function ImageUpload() {
+    const navigate=useNavigate();
+    const {update}=useContext(UserContext);
+    const token=sessionStorage.getItem('usertoken');
+    const [images,setImages]=useState([]);
+
+    useEffect(()=>{
+        if(!token){
+            navigate('/login');
+        }
+    },[token]);
+
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        const formData=new FormData();
+        for(let i=0;i<images.length;i++){
+            formData.append("file"+i,images[i]);
+        }
+
+    }
+
+    const kuldes=(adat)=>{
+        fetch(`${import.meta.env.VITE_BASE_URL}/api/files/upload`,{
+            method:'POST',
+            headers:{
+                "Authorization":`Bearer ${token}`
+            },
+            body:adat
+        })
+        .then(res=>res.json())
+        .then(valasz=>alert(valasz.message))
+        .catch(err=>alert(err));
+
+    }
+
+    const imageChange=(e)=>{
+        setImages([...images,...e.target.files])
+    }
+
+
   return (
-    <div>ImageUpload</div>
+    <div>
+        <h2 className="text-2xl font-bold text-center text-sky-800">Képek feltöltése</h2>
+        <div className="flex flex-col items-center justify-center">
+            <form onSubmit={onSubmit}>
+                <input onChange={imageChange} className="file-input file-input-primary" type="file" multiple required />
+                <div>
+                    <button type="submit" className="btn btn-primary m-10">Feltöltés</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
   )
 }
 
