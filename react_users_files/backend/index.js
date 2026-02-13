27636543359af28c3641bcc55a83f1ba4200e3a8
kovacsrud <kovacs.rud@gmail.com>
2026-02-13 09:18:17 +0100

    Backend funkciók a bináris képkezeléshez készek

--- react_users_files/backend/index.js
+++ react_users_files/backend/index.js
@@ -16,6 +16,7 @@ app.use('/files',express.static(path.join(__dirname,'files')));
 app.use('/api/user',require('./routes/userRoutes'));
 app.use('/api/files',require('./routes/uploadRoutes'));
 app.use('/api/images',require('./routes/imageRoutes'));
+app.use('/api/imagesbin',require('./routes/imageBinRoutes'));
 
 mongoose.connect("mongodb://localhost:27017/users_2025")
 .then(()=>console.log("Kapcsolódva"))
