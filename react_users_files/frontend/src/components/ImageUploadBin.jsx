51720bc5f8eb338c1363e02b4875fe0bbe47d736
kovacsrud <kovacs.rud@gmail.com>
2026-02-13 10:17:34 +0100

    Bináris feltöltés rendben

--- react_users_files/frontend/src/components/ImageUploadBin.jsx
+++ react_users_files/frontend/src/components/ImageUploadBin.jsx
@@ -1,8 +1,58 @@
-
+import { useState,useContext,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../context/UserContext";
 
 function ImageUploadBin() {
+    const {update}=useContext(UserContext);
+    const navigate=useNavigate();
+    const [images,setImages]=useState([]);
+    const token=sessionStorage.getItem('usertoken');
+
+    useEffect(()=>{
+        if(!token){
+            navigate('/login');
+        }
+    },[token]);
+
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        const formData=new FormData();
+        for(let i=0;i<images.length;i++){
+            formData.append("file"+i,images[i]);
+        }
+        kuldes(formData);
+        navigate('/imagesbin');
+    }
+
+    const kuldes=(adat)=>{
+        fetch(`${import.meta.env.VITE_BASE_URL}/api/imagesbin`,{
+            method:'POST',
+            headers:{
+                "Authorization":`Bearer ${token}`
+            },
+            body:adat
+        })
+        .then(res=>res.json())
+        .then(valasz=>{alert(valasz.message);update()})
+        .catch(err=>alert(err));
+    }
+
+    const imageChange=(e)=>{
+        setImages([...images,...e.target.files])
+    }
+
   return (
-    <div>ImageUploadBin</div>
+    <div>
+        <h2 className="m-10 text-2xl font-bold text-center text-sky-800">Képek feltöltése (bin)</h2>
+        <div className="flex flex-col items-center justify-center">
+            <form onSubmit={onSubmit}>
+                <input onChange={imageChange} className="file-input file-input-primary" type="file" multiple required/>
+                <div>
+                  <button className="btn btn-primary">Feltöltés</button>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
 
