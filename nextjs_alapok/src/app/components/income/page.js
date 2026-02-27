ed89f8d9ced1d7091581cf04ce38e3a26aced76f
kovacsrud <kovacs.rud@gmail.com>
2026-02-27 11:49:37 +0100

    Update page.js

--- nextjs_alapok/src/app/components/income/page.js
+++ nextjs_alapok/src/app/components/income/page.js
@@ -1,16 +1,25 @@
 'use client'
 import { useState } from "react";
+import IncomeCard from "./incomecard/page";
 
 
 export default function Income(){
     const [also,setAlso]=useState(0);
     const [felso,setFelso]=useState(0);
     const [customers,setCustomers]=useState([]);
+    const [message,setMessage]=useState("");
 
     const getData=()=>{
         fetch(`http://localhost:3000/api/customers/income2?also=${also}&felso=${felso}`)
         .then(res=>res.json())
-        .then(adat=>{setCustomers(adat)})
+        .then(adat=>{
+            if(adat.message){
+                setMessage(adat.message);
+            } else {
+                setCustomers(adat);
+            }
+            
+        })
         .catch(err=>alert(err));
     }
 
@@ -25,9 +34,9 @@ export default function Income(){
             <div className="flex flex-row flex-wrap items-center justify-center">
                 {
                     customers.length>0 ?
-                     customers.map((customer)=>(<p key={customer.customer_id}>{customer.first_name} {customer.last_name}</p>))
+                     customers.map((customer)=>(<IncomeCard key={customer.customer_id} customer={customer}/>))
                       :
-                     <p className="text-center font-bold text-2xl">Nincs a feltételeknek megfelelő adat!</p>
+                     <p className="text-center font-bold text-2xl">{message}</p>
 
                 }
             </div>
