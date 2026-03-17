dff779d9cc88e7ec5dfa0602b3565aff3bbe37b7
kovacsrud <kovacs.rud@gmail.com>
2026-03-17 10:39:56 +0100

    Update App.vue

--- vue_alapok/src/App.vue
+++ vue_alapok/src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue';
-import Posts from './components/Posts.vue';
 import Menu from './components/Menu.vue';
 import {RouterView} from 'vue-router';
 </script>
